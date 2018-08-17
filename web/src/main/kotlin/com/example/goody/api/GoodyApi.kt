098ece88de5e58ef83c7122afad347aa775866a1
Chris Rankin <chris.rankin@r3.com>
2018-08-17 16:54:56 +0100

    Add asynchronous Web APIs for Issue and Transfer flows.

--- web/src/main/kotlin/com/example/goody/api/GoodyApi.kt
+++ web/src/main/kotlin/com/example/goody/api/GoodyApi.kt
@@ -1,6 +1,7 @@
 package com.example.goody.api
 
 import com.example.goody.flows.GoodyIssueFlow
+import com.example.goody.flows.GoodyTransferFlow
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.identity.Party
 import net.corda.core.messaging.CordaRPCOps
@@ -8,7 +9,6 @@ import net.corda.core.messaging.startFlow
 import net.corda.core.utilities.getOrThrow
 import net.corda.core.utilities.loggerFor
 import javax.ws.rs.*
-import javax.ws.rs.client.InvocationCallback
 import javax.ws.rs.container.AsyncResponse
 import javax.ws.rs.container.CompletionCallback
 import javax.ws.rs.container.Suspended
@@ -32,35 +32,48 @@ class GoodyApi(private val rpcOps: CordaRPCOps) {
     @Produces(APPLICATION_JSON)
     fun whoami() = mapOf("me" to myLegalName)
 
+    /**
+     * Submits an "Issue" request for more Candy.
+     */
     @POST
     @Path("issue")
     @Consumes(APPLICATION_JSON)
-    fun issue(request: IssueRequest?): Response {
-        val issueRequest = request ?: throw BadRequestException("Request data missing")
-        log.info("Received Issue request: candy='{}', reference={}", issueRequest.candy, issueRequest.issuerReference)
-        return try {
-            val signedTx = rpcOps.startFlow(::GoodyIssueFlow, issueRequest.candy, issueRequest.issuerReference, rpcOps.notaryIdentities().first())
-                    .returnValue.getOrThrow()
-            Response.ok("Transaction ID: ${signedTx.id}").build()
-        } catch (ex: Exception) {
-            log.error("Issue failed: ${ex.message}", ex)
-            throw BadRequestException(ex.message)
-        }
-    }
-
-    @POST
-    @Path("issueAsync")
-    @Consumes(APPLICATION_JSON)
-    fun asyncIssue(request: IssueRequest?, @Suspended async: AsyncResponse) {
-        val issueRequest = request ?: throw BadRequestException("Request data missing")
-        log.info("Received Issue request: candy='{}', reference={}", issueRequest.candy, issueRequest.issuerReference)
+    fun issue(request: IssueRequest?, @Suspended async: AsyncResponse) {
         async.register(CompletionCallback { ex ->
-            if (ex == null) log.info("Issue complete") else log.error("Issue failed", ex)
+            if (ex == null) log.info("Issue completed") else log.error("Issue failed", ex)
         })
+        val issueRequest = request ?: throw BadRequestException("Request data missing")
+        log.info("Received Issue request: candy='{}', reference={}", issueRequest.candy, issueRequest.issuerReference)
 
         thread(isDaemon = true) {
             try {
-                val signedTx = rpcOps.startFlow(::GoodyIssueFlow, issueRequest.candy, issueRequest.issuerReference, rpcOps.notaryIdentities().first())
+                val notary = issueRequest.notary ?: rpcOps.notaryIdentities().first()
+                val signedTx = rpcOps.startFlow(::GoodyIssueFlow, issueRequest.candy, issueRequest.issuerReference, notary)
+                        .returnValue.getOrThrow()
+                async.resume(Response.ok("Transaction ID: ${signedTx.id}").build())
+            } catch (ex: Exception) {
+                async.resume(BadRequestException(ex.message))
+            }
+        }
+    }
+
+    /**
+     * Submits a "Transfer" request for some Candy to another party.
+     */
+    @POST
+    @Path("transfer")
+    @Consumes(APPLICATION_JSON)
+    fun transfer(request: TransferRequest?, @Suspended async: AsyncResponse) {
+        async.register(CompletionCallback { ex ->
+            if (ex == null) log.info("Transfer completed") else log.error("Transfer failed", ex)
+        })
+
+        val transferRequest = request ?: throw BadRequestException("Request data missing")
+        log.info("Received Transfer request: candy='{}', recipient='{}'", transferRequest.candy, transferRequest.recipient)
+
+        thread(isDaemon = true) {
+            try {
+                val signedTx = rpcOps.startFlow(::GoodyTransferFlow, transferRequest.candy, transferRequest.recipient)
                         .returnValue.getOrThrow()
                 async.resume(Response.ok("Transaction ID: ${signedTx.id}").build())
             } catch (ex: Exception) {
--- web/src/main/kotlin/com/example/goody/api/TransferRequest.kt
+++ web/src/main/kotlin/com/example/goody/api/TransferRequest.kt
@@ -4,15 +4,11 @@ import com.example.goody.contracts.Candy
 import com.fasterxml.jackson.annotation.JsonProperty
 import net.corda.core.contracts.Amount
 import net.corda.core.identity.Party
-import net.corda.core.utilities.OpaqueBytes
 
-class IssueRequest(
+class TransferRequest(
     @JsonProperty("candy")
     val candy: Amount<Candy>,
 
-    @JsonProperty("issuerReference")
-    val issuerReference: OpaqueBytes,
-
-    @JsonProperty("notary")
-    val notary: Party?
+    @JsonProperty("recipient")
+    val recipient: Party
 )
